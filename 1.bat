89b17166e3619aff99f6ada6ecf2a4fe0b6f8302
EmmanuilZorg <mister.zorg.68@gmail.com>
2025-06-15 23:47:40 +0200

    init

---
+++ 1.bat
@@ -0,0 +1,26 @@
+﻿@echo off
+setlocal
+
+:: === Настройки ===
+set REPO_URL=https://github.com/EmmanuilZorg/lj-rss.git
+
+echo Инициализация git репозитория...
+git init
+
+echo Добавление файлов...
+git add .
+
+echo Первый коммит...
+git commit -m "init"
+
+echo Переименование ветки в main...
+git branch -M main
+
+echo Добавление origin...
+git remote add origin %REPO_URL%
+
+echo Пуш в GitHub...
+git push -u origin main
+
+echo Готово.
+pause
